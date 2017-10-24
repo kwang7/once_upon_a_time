89bfe1354d44fe288d212a290de3e06aeb72ef2a
Karina Ionkina <kionkina@stuy.edu>
2017-10-24 16:06:08 -0400

    additions to stotycode and devlog

--- storycode.py
+++ storycode.py
@@ -9,8 +9,23 @@ def add_story(story_id, story_name):
     db = sqlite3.connect(db_builder.DATABASE)
     c = db.cursor()
     cmd = "INSERT INTO stories(id, name) VALUES ("  + str(story_id) +  ", '" + story_name + "'" + ")"
-    print cmd
-
-
-add_story(1, "One day...")
+    c.execute(cmd)
+    db.commit()
+    db.close()
     
+
+
+#add_story(1, "One day...")
+#add_story(2, "Anotha day...")
+
+    
+def see_table(table):
+    db = sqlite3.connect(db_builder.DATABASE)
+    c = db.cursor()
+    cmd = "SELECT * FROM " + table
+    ret = c.execute(cmd)
+    for thing in ret:
+        print thing
+    db.close()
+    
+see_table("stories")
