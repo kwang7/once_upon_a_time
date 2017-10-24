f184b1e6093cdc26bac1b141ba402a30d134a875
Helen Ye <00yhelen@gmail.com>
2017-10-24 13:02:30 -0400

    Add basic db builder

---
+++ db_builder.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+DATABASE = "once_upon_a_time.db"
+
+def create_tables():
+    db = sqlite3.connect(DATABASE)
+
+    c.execute("CREATE TABLE IF NOT EXISTS stories (id INTEGER PRIMARY KEY, name TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS edits (story_id INTEGER, user_id INTEGER, timestamp INTEGER, content TEXT)")
+
+    db.commit()
+    db.close()
