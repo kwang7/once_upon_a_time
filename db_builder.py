dc349c64545bc318ea72228696428fa5e5d75f26
Helen Ye <00yhelen@gmail.com>
2017-10-24 16:12:46 -0400

    Add editing and putting stories together

--- db_builder.py
+++ db_builder.py
@@ -5,8 +5,8 @@ DATABASE = "once_upon_a_time.db"
 def create_tables():
     db = sqlite3.connect(DATABASE)
     c = db.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS stories (id INTEGER PRIMARY KEY, name TEXT)")
-    c.execute("CREATE TABLE IF NOT EXISTS edits (story_id INTEGER, user_id INTEGER, timestamp INTEGER, content TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS stories (id INTEGER PRIMARY KEY, name TEXT NOT NULL)")
+    c.execute("CREATE TABLE IF NOT EXISTS edits (story_id INTEGER NOT NULL, user_id INTEGER NOT NULL, timestamp INTEGER NOT NULL, content TEXT NOT NULL)")
 
     db.commit()
     db.close()
--- storycode.py
+++ storycode.py
@@ -1,7 +1,6 @@
 import sqlite3
 import db_builder
-
-
+import datetime
 
 db_builder.create_tables()
 
@@ -12,13 +11,9 @@ def add_story(story_id, story_name):
     c.execute(cmd)
     db.commit()
     db.close()
-    
-
-
 #add_story(1, "One day...")
 #add_story(2, "Anotha day...")
 
-    
 def see_table(table):
     db = sqlite3.connect(db_builder.DATABASE)
     c = db.cursor()
@@ -27,5 +22,29 @@ def see_table(table):
     for thing in ret:
         print thing
     db.close()
-    
+
 see_table("stories")
+
+def add_edit(story_id, user_id, timestamp, content):
+    db = sqlite3.connect(db_builder.DATABASE)
+    c = db.cursor()
+    formatted_time = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    cmd = "INSERT INTO edits VALUES (" + str(story_id) + "," + str(user_id) + ", '" + \
+            formatted_time + "', '" + content + "')"
+    c.execute(cmd)
+    db.commit()
+    db.close()
+
+def get_story(story_id):
+    db = sqlite3.connect(db_builder.DATABASE)
+    c = db.cursor()
+    query = "SELECT * FROM edits WHERE story_id =" + str(story_id) + " ORDER BY timestamp ASC"
+    result = c.execute(query)
+    for row in result:
+        print row
+
+add_story(1,"Title")
+add_edit(1,1,datetime.datetime.today(),"Edit 1")
+add_edit(1,2,datetime.datetime.today(),"Edit 2")
+add_edit(1,3,datetime.datetime.today(),"Edit 3")
+get_story(1)
