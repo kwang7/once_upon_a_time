7246fe90ca07095b2d5cbd4ce668b8cc509dd425
Helen Ye <00yhelen@gmail.com>
2017-10-25 13:07:47 -0400

    Clean code and change column name

--- storycode.py
+++ storycode.py
@@ -6,12 +6,13 @@ db_builder.create_tables()
 DATABASE = db_builder.DATABASE
 
 def add_story(story_id, story_name):
-    db = sqlite3.connect(db_builder)
+    db = sqlite3.connect(DATABASE)
     c = db.cursor()
     cmd = "INSERT INTO stories(id, title) VALUES ("  + str(story_id) +  ", '" + story_name + "'" + ")"
     c.execute(cmd)
     db.commit()
     db.close()
+
 #add_story(1, "One day...")
 #add_story(2, "Anotha day...")
 
@@ -24,8 +25,9 @@ def see_table(table):
         print thing
     db.close()
 
-see_table("stories")
-print("\n")
+# see_table("stories")
+# print("\n")
+
 def add_edit(story_id, user_id, timestamp, content):
     db = sqlite3.connect(DATABASE)
     c = db.cursor()
@@ -40,22 +42,15 @@ def get_story(story_id):
     db = sqlite3.connect(DATABASE)
     c = db.cursor()
     query = "SELECT * FROM edits WHERE story_id =" + str(story_id) + " ORDER BY timestamp ASC"
-    result = c.execute(query)
-    if result:
-        return result
+    story = c.execute(query)
+    db.close()
+    if story:
+        return story
     return []
 
-<<<<<<< Updated upstream
-#add_story(1,"Title")
-add_edit(1,1,datetime.datetime.today(),"Edit 1")
-add_edit(1,2,datetime.datetime.today(),"Edit 2")
-add_edit(1,3,datetime.datetime.today(),"Edit 3")
-get_story(1)
-=======
 # add_story(1,"Title")
 # add_edit(1,1,datetime.datetime.today(),"Edit 1")
 # add_edit(1,2,datetime.datetime.today(),"Edit 2")
 # add_edit(1,3,datetime.datetime.today(),"Edit 3")
-for row in get_story(1):
-    print row
->>>>>>> Stashed changes
+# for row in get_story(1):
+#     print row
